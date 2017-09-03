6aa1fc33e4b8feb62af9c84249403ee6acc96640
Yura Oleynik <Gostwel@gmail.com>
2017-09-03 11:57:44 +0300

    Optimization

--- FuckStatistic/Model/Game.cs
+++ FuckStatistic/Model/Game.cs
@@ -36,12 +36,13 @@ namespace FuckStatistic.Model
             int firstPosition = RandomHelper.Rand(_map.Slots.Length);
             _map.ChooseSlot(firstPosition);
 
-            var badSlot = _map.Slots.First(x => !x.HasPrize && x.Position != firstPosition);
-            _map.ChooseSlot(badSlot.Position);
-
             // Если меня меняем выбор
             if (ChangeChoise)
             {
+                // Показываем где плохой вариант
+				var badSlot = _map.Slots.First(x => !x.HasPrize && x.Position != firstPosition);
+				_map.ChooseSlot(badSlot.Position);
+
                 // Тогда выбираем слот которые не равен первому выбору и который нам показали
                 var selectedSlot = _map.Slots.First(x => x.Position != firstPosition && x.Position != badSlot.Position);
                 _map.OpenSlot(selectedSlot.Position);
